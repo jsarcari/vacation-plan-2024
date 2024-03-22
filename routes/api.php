97ac330f7d925f1a0dc0d88fdea518eb9c273f38
Juan Dos Santos Arcari <Juan.Arcari@zaffari.com.br>
2024-03-22 13:56:43 -0300

    CRUD API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\HolidayPlan;
+use App\Http\Requests\VacationsFormRequest;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Access in http://localhost:8000/api/vacations as method GET
+Route::get('/vacations', function () {
+    return HolidayPlan::all();
+});
+
+// Access in http://localhost:8000/api/vacations as method POST, sending a new HolidayPlan in JSON format
+Route::post('/vacations', function (VacationsFormRequest $request) {
+    return response()->json(HolidayPlan::create($request->all()));
+});
+
+Route::get('/vacations/{id}', function ($id) {
+    return response()->json(HolidayPlan::find($id));
+});
+
+Route::put('/vacations/{vacation}', function (HolidayPlan $vacation, VacationsFormRequest $request) {
+    $vacation->fill($request->except('_token'));
+    $vacation->save();
+    return $vacation;
+});
+
+Route::delete('/vacations/{id}', function ($id) {
+    HolidayPlan::destroy($id);
+    return response()->noContent();
 });
